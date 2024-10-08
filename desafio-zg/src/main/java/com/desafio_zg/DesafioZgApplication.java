b0767946717e1b0d441d19f217138fa8e0ca6797
JoaolimaDev <ozymandiasphp@gmail.com>
2024-10-08 13:53:11 -0300

    Primerio commit

---
+++ desafio-zg/src/main/java/com/desafio_zg/DesafioZgApplication.java
@@ -0,0 +1,13 @@
+package com.desafio_zg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioZgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioZgApplication.class, args);
+	}
+
+}
---
+++ desafio-zg/src/test/java/com/desafio_zg/DesafioZgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio_zg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioZgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
